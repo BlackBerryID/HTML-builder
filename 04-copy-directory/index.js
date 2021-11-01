0080584ec72cfbced82f35823cb55f2112f7f4f2
BlackBerryID <iddf1994one@gmail.com>
2021-11-01 21:37:46 +0300

    feat: solve 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const { copyFile, mkdir, rmdir, readdir } = require("fs/promises");
+const path = require("path");
+const dirPath = path.join(__dirname, "files");
 
+async function cloneFilesFromDir() {
+  await rmdir(`04-copy-directory/files-copy`, { recursive: true }, (err) => {
+    if (err) {
+      console.error(error.message);
+    }
+  });
+  await mkdir(`04-copy-directory/files-copy`, { recursive: true }, (err) => {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+  const files = await readdir(dirPath);
+  for (const file of files) {
+    await copyFile(
+      `04-copy-directory/files/${file}`,
+      `04-copy-directory/files-copy/${file}`
+    );
+  }
+}
+
+cloneFilesFromDir();
