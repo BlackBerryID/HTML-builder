566032c8cd77573b773683055e4f0f3729fc4527
BlackBerryID <iddf1994one@gmail.com>
2021-11-01 20:19:48 +0300

    feat: solve 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const { readdir } = require("fs/promises");
+const { stat } = require("fs");
+const path = require("path");
+const dirPath = path.join(__dirname, "secret-folder");
+
+async function displayFilesFromDirectory() {
+  const files = await readdir(dirPath);
+  for (const file of files) {
+    stat(path.join(dirPath, file), (err, stats) => {
+      if (err) return console.error(err.message);
+      if (stats.isFile()) {
+        let fileParse = path.parse(file);
+        console.log(
+          `${fileParse.name}-${fileParse.ext.replace(".", "")}-${
+            stats.size / 1024
+          }kb`
+        );
+      }
+    });
+  }
+}
+
+displayFilesFromDirectory();
